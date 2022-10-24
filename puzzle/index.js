c11af6e4b52bd368e7cb60db4ca9533f328af9bd
Ina Klimenka <innazolotkomoe@gmail.com>
2022-10-24 22:17:02 +0200

    feat: added swap function

--- puzzle/index.js
+++ puzzle/index.js
@@ -1,7 +1,6 @@
 const gameContainer = document.querySelector('.game-container');
 const cell = Array.from(document.querySelectorAll('.cell'));
-const button = document.querySelector('.repeat');
-
+const repeat = document.querySelector('.repeat');
 
 
 function getRandom () {
@@ -10,9 +9,36 @@ function getRandom () {
     .forEach( elem => gameContainer.appendChild(elem));
 }
 
-button.addEventListener('click', () => getRandom())
+repeat.addEventListener('click', () => getRandom())
 
+gameContainer.addEventListener('click', (event) => {
+    const button = event.target.closest('.cell');
+    if(!button){
+        return
+    }
+    changeCell(button)
+})
 
-function changeCell () {
-    
+function changeCell (button) {
+    let buttonNullIndex = -1;
+    let buttonIndex = -1;
+    let btnToNull = {};
+
+    for(let i = 0; i < cell.length; i++){
+        if(cell[i] === button){
+            buttonIndex = i
+        };
+        if(cell[i].textContent === ''){
+            buttonNullIndex = i
+        }
+    }
+
+    btnToNull = cell[buttonNullIndex];
+    cell[buttonNullIndex] = cell[buttonIndex];
+    cell[buttonIndex] = btnToNull;
+
+    gameContainer.innerHTML = ''; 
+    cell.forEach(elem => gameContainer.appendChild(elem))
+
 }
+
