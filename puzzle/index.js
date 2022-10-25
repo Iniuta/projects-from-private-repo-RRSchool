dd773da593639e4bf221952ded8f33fd248c3b6e
Ina Klimenka <innazolotkomoe@gmail.com>
2022-10-25 15:56:43 +0200

    feat: added audio and changeCell function

--- puzzle/index.js
+++ puzzle/index.js
@@ -1,44 +1,84 @@
-const gameContainer = document.querySelector('.game-container');
-const cell = Array.from(document.querySelectorAll('.cell'));
-const repeat = document.querySelector('.repeat');
+const gameContainer = document.querySelector(".game-container");
+const repeat = document.querySelector(".repeat");
+
+let topDownNeighbor = 4;
 
 
-function getRandom () {
-    gameContainer.innerHTML = ''; 
-    cell.sort(() => Math.random() - 0.5)
-    .forEach( elem => gameContainer.appendChild(elem));
+for (let i = 1; i <= 15; i++) {
+  const cell = document.createElement("button");
+  cell.className = "cell";
+  cell.innerHTML = i;
+
+  gameContainer.append(cell);
+
 }
 
-repeat.addEventListener('click', () => getRandom())
 
-gameContainer.addEventListener('click', (event) => {
-    const button = event.target.closest('.cell');
-    if(!button){
-        return
+const cellNull = document.createElement("button");
+cellNull.className = "cell-null cell";
+cellNull.innerHTML = "";
+
+gameContainer.append(cellNull);
+
+const cell = Array.from(document.querySelectorAll(".cell"));
+
+function soundRepeat() {
+    const audio = new Audio(); 
+    audio.src = `audio/swap.mp3`; 
+    audio.autoplay = true; 
+  }
+
+function getRandom() {
+  gameContainer.innerHTML = "";
+  cell.sort(() => Math.random() - 0.5).forEach((elem) => gameContainer.appendChild(elem));
+}
+getRandom();
+
+repeat.addEventListener("click", () => getRandom());
+repeat.addEventListener("click", () => soundRepeat());
+
+gameContainer.addEventListener("click", (event) => {
+  const button = event.target.closest(".cell");
+  if (!button) {
+    return;
+  }
+  changeCell(button);
+});
+
+
+function soundClick() {
+  const audio = new Audio(); 
+  audio.src = `audio/fefteen.mp3`; 
+  audio.autoplay = true; 
+}
+
+
+function changeCell(button) {
+  let buttonNullIndex = -1;
+  let buttonIndex = -1;
+  let btnToNull = {};
+
+  for (let i = 0; i < cell.length; i++) {
+    if (cell[i] === button) {
+      buttonIndex = i;
     }
-    changeCell(button)
-})
-
-function changeCell (button) {
-    let buttonNullIndex = -1;
-    let buttonIndex = -1;
-    let btnToNull = {};
-
-    for(let i = 0; i < cell.length; i++){
-        if(cell[i] === button){
-            buttonIndex = i
-        };
-        if(cell[i].textContent === ''){
-            buttonNullIndex = i
-        }
+    if (cell[i].textContent === "") {
+      buttonNullIndex = i;
     }
+  }
 
+  if (
+    Math.abs(buttonNullIndex - buttonIndex) === 1 ||
+    Math.abs(buttonNullIndex - buttonIndex) === topDownNeighbor
+  ) {
     btnToNull = cell[buttonNullIndex];
     cell[buttonNullIndex] = cell[buttonIndex];
     cell[buttonIndex] = btnToNull;
 
-    gameContainer.innerHTML = ''; 
-    cell.forEach(elem => gameContainer.appendChild(elem))
+    gameContainer.innerHTML = "";
+    cell.forEach((elem) => gameContainer.appendChild(elem));
+    soundClick()
+  }
+  
 
 }
-
